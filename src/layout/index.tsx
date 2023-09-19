c09cda00d7da2bf8b396a6960e3d8cc479df00b5
Danielle Goodlake <daniellegoodlake@icloud.com>
2023-09-19 18:23:59 +0100

    Adapt basket button text - go to menu if in basket

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,11 +4,28 @@ import { useNavigate } from "react-router-dom";
 const Layout: React.FC<{children: React.ReactNode}> = ({children}): JSX.Element => {
     
     const navigate = useNavigate();
+
+
+    let currentlyInBasket: boolean = window.location.href.endsWith("basket");
+
+
+    const handleClick = () => {
+        if (currentlyInBasket)
+        {
+            navigate("/restaurant-menu/");
+        }
+        else
+        {
+            navigate("/restaurant-menu/basket")
+        }
+    }
+
+    let buttonText = currentlyInBasket? "Return to Menu" : "View Basket";
     
     return (<main>
         <header>
             <h1 className="custom-navbar">Welcome to Restaurant Menu Browser</h1>
-            <button className="btn btn-lg btn-dark" onClick={() => navigate("/restaurant-menu/basket")}>View Basket</button>
+            <button id="btn-view-basket" className="btn btn-lg btn-dark" onClick={() => handleClick()}>{buttonText}</button>
         </header>
         <section>
             {children}
